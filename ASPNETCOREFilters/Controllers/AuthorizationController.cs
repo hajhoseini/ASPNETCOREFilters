f0596d53f5ed6f753f166bcc59c0f81c84b0e825
Azam HajMohammadHoseini <hoseini.1988@gmail.com>
2024-08-07 14:41:21 -0700

    example for AuthorizationFilter

---
+++ ASPNETCOREFilters/Controllers/AuthorizationController.cs
@@ -0,0 +1,20 @@
+﻿using ASPNETCOREFilters.Models.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCOREFilters.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [CustomAuthorizationFilter("admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [CustomAuthorizationFilter("customer")]
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPNETCOREFilters/Models/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace ASPNETCOREFilters.Models.Filters
+{
+    public class CustomAuthorizationFilter : Attribute, IAuthorizationFilter
+    {
+        private readonly string _role;
+
+        public CustomAuthorizationFilter(string role)
+        {
+            _role = role;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var isAuthorized = CheckPermission(context.HttpContext.User, _role);
+            if (!isAuthorized)
+                context.Result = new UnauthorizedResult();
+        }
+
+        private bool CheckPermission(ClaimsPrincipal principal, string role)
+        {
+            //check conditions ...
+
+            if (role == "admin")
+                return true;
+
+            return false;
+        }
+    }
+}
